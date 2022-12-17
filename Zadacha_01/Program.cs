64d9f0784c27bc04f26f28676fb20c2eed488e06
dmitriy <dima-dim@yandex.ru>
2022-12-17 13:40:53 +0300

    Решение 1 задачи

--- Zadacha_01/Program.cs
+++ Zadacha_01/Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Введите первое  число ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a == b){
+	Console.WriteLine("Вы написали одинаковые числа");
+}
+else{
+	if (a > b)
+	{
+		Console.WriteLine("Максимальное число: " + a + " Минимальное число: " + b);
+	}
+	else
+	{
+		Console.WriteLine("Самое большое число здесь: " + b + " Минимальное число: " + a);
+	}
+}
